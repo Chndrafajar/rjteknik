ef695cf88f53d3ce77d33bc0662f60b7baaa2845
InstaCodex <chandrafajar356@gmail.com>
2024-01-11 14:11:04 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var swiper = new Swiper('.mySwiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    430: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    940: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  // Optional parameters
+
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
